c9d6a3c647c90a55c4cbe6e250cbae3277e41d89
Egidio Santos <1130348@isep.ipp.pt>
2015-06-02 17:23:54 +0100

    CSDH-79: Análise do caso de uso CRM01.1 - Edição de Contacto Criação de Testes

---
+++ src-tests/csheets/crm/LoginUserTest.java
@@ -0,0 +1,30 @@
+package csheets.crm;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * A Unit Test class to test LoginUser.
+ *
+ * @see LoginUser
+ * @author Egidio Santos
+ */
+public class LoginUserTest {
+
+    /**
+     * A method that tests if the User can login.
+     */
+//    @Test
+//    public void testLogin() {
+//
+//        LoginUser loginUser = new LoginUser();
+//        String name = "Teste";
+//
+//        boolean expResult = true;
+//        boolean result = loginUser.login(name);
+//
+//        assertEquals(expResult, result);
+//
+//    }
+
+}
---
+++ src-tests/csheets/crm/RemoveContactTest.java
@@ -0,0 +1,30 @@
+package csheets.crm;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * A Unit Test class to test RemoveContact.
+ *
+ * @see RemoveContact
+ * @author Egidio Santos
+ */
+public class RemoveContactTest {
+
+    /**
+     * A method that tests if the contact to be removed isn't null.
+     */
+//    @Test
+//    public void testRemoveContact() {
+//
+//        RemoveContact removeContact = new RemoveContact();
+//        Contact contact = null;
+//        
+//        boolean expResult=false;
+//        boolean result=removeContact.removeContact(contact);
+//        
+//        assertEquals(expResult, result);
+//
+//    }
+
+}
