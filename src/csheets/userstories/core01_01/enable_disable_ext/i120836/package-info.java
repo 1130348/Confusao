785d877e923d623587adbd6532a77fec544cfd2a
Marcos <Marcos@Dragon-PC.dei.isep.ipp.pt>
2015-06-03 12:41:16 +0100

    Core01_01 Enable Disable Extensions -> Analysis

---
+++ src/csheets/userstories/core01_01/enable_disable_ext/i120836/package-info.java
@@ -0,0 +1,50 @@
+/**
+ * Technical documentation regarding the user story core01_1: enable/disable
+ * extensions.
+ * <br/>
+ * <br/>
+ *
+ * <h2>1. Requirements</h2>
+ * Provide a window to allow enable and disable cleansheets extensions. It
+ * should also be a sidebar window "Navigator" to submit extensions (enable and
+ * disable) .
+ * <br/>
+ * <br/>
+ * <b>Use Case "Enable/Disable":</b> The user selects the extension on the
+ * "Navigator" tab where he can disable or enable the extension desired.<br/>
+ * <br/>
+ *
+ * <h2>2. Analysis</h2>
+ * The UC Core01.1 objective is to organize the extensions in the
+ * application.<br />
+ * The application will have a SideBar "Navigator" that will show the all the
+ * extensions giving the chance to modify the status (enable/disable) of the
+ * extension. The extension(s) that received a modify(disable) to it's status
+ * will appear "locked" and if enable will allow to be used. For the development
+ * of the this functionality we will use a few classes ans their properties that
+ * already has been created like: "ExtensionManager" and "Extension".
+ * <br/>
+ *
+ * <h2>a) First "Analysis" Sequence Diagram</h2>
+ * <img src="doc-files/core01_01_enable_disable_ext_design_analysis.png">
+ *
+ * <h2>5. Coding</h2>
+ * see:
+ * <br/>
+ * <br/>
+ * <br/>
+ *
+ * @author 1120836
+ *
+ */
+package csheets.userstories.core01_01.enable_disable_ext.i120836;
+
+/**
+ * This class is only here so that javadoc includes the documentation about this
+ * EMPTY package! Do not remove this class!
+ *
+ * @author 1120836
+ *
+ */
+class _Dummy_ {
+}
