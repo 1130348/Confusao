5ec88ecd78022d103c974c41d2973542e506411f
João Monteiro <1130372@isep.ipp.pt>
2015-06-04 14:02:52 +0100

    CSDH - 129 #time 2h #resolve Plano de testes

--- src/csheets/userstories/ipc01_01/start_sharing/i130372/package-info.java
+++ src/csheets/userstories/ipc01_01/start_sharing/i130372/package-info.java
@@ -57,77 +57,18 @@
  * Because of the <code>Extensible</code> it seams that a cell can be
  * extended.<br/>
  *
- * <h2>5. Coding</h2>
- * see:<br/>
- * <a href="../../../../csheets/ext/comments/package-summary.html">csheets.ext.comments</a><br/>
- * <a href="../../../../csheets/ext/comments/ui/package-summary.html">csheets.ext.comments.ui</a><br/>
- * <br/>
- * <h2>6. Final Remarks</h2>
- * As an extra this use case also implements a small cell visual decorator if
- * the ce If we further investigate the hierarchy of {@link csheets.core.Cell}
- * we see that it has a subclass {@link csheets.ext.CellExtension} which has a
- * subclass {@link csheets.ext.style.StylableCell}.
- * {@link csheets.ext.style.StylableCell} seems to be an example of how to
- * extend cells.<br/>
- * Therefore, we will assume that it is possible to extend cells and start to
- * implement tests for this use case. <br/>
- * <br/>
- * The <a href="http://en.wikipedia.org/wiki/Delegation_pattern">delegation
- * design pattern</a> is used in the cell extension mechanism of cleansheets.
- * The following class diagram depicts the relations between classes in the
- * "Cell" hierarchy.<br/>
- * <img src="doc-files/core02_01_analysis_cell_delegate.png">
- * <br/>
- *
- * One important aspect is how extensions are dynamically created and returned.
- * The <code>Extensible</code> interface has only one method,
- * <code>getExtension</code>. Any class, to be extensible, must return a
- * specific extension by its name. The default (and base) implementation for the
- * <code>Cell</code> interface, the class <code>CellImpl</code>, implements the
- * method in the following manner:<br/>
- * <pre>
- * {@code
- * 	public Cell getExtension(String name) {
- *		// Looks for an existing cell extension
- *		CellExtension extension = extensions.get(name);
- *		if (extension == null) {
- *			// Creates a new cell extension
- *			Extension x = ExtensionManager.getInstance().getExtension(name);
- *			if (x != null) {
- *				extension = x.extend(this);
- *				if (extension != null)
- *					extensions.put(name, extension);
- *			}
- *		}
- *		return extension;
- *	}
- * }
- * </pre> As we can see from the code, if we are requesting a extension that is
- * not already present in the cell, it is applied at the moment and then
- * returned. The extension class (that implements the <code>Extension</code>
- * interface) what will do is to create a new instance of its cell extension
- * class (this will be the <b>delegator</b> in the pattern). The constructor
- * receives the instance of the cell to extend (the <b>delegate</b> in the
- * pattern). For instance, <code>StylableCell</code> (the delegator) will
- * delegate to <code>CellImpl</code> all the method invocations regarding
- * methods of the <code>Cell</code> interface. Obviously, methods specific to
- * <code>StylableCell</code> must be implemented by it.<br/>
- * Therefore, to implement a cell that can have a associated comment we need to
- * implement a class similar to <code>StylableCell</code>.<br/>
  * <h2>3. Tests</h2>
  * Basically, from requirements and also analysis, we see that the core
- * functionality of this use case is to be able to add an attribute to cells to
- * be used to store a comment/text. We need to be able to set and get its
- * value.<br/>
- * Following this approach we can start by coding a unit test that uses a
- * subclass of <code>CellExtension</code> with a new attribute for user comments
- * with the corresponding method accessors (set and get). A simple test can be
- * to set this attribute with a simple string and to verify if the get method
- * returns the same string.<br/>
+ * functionality of this use case is to be able communicate with other instances
+ * of "CleanSheets" and send them a selected amount of cells.<br/>
+ * Following this approach we can start by coding a unit test that uses the
+ * class <code>Connect</code> and tests its attribute port. Furthermore we also
+ * have to perform functional tests to see if the client is properly receiving
+ * the information.<br/>
  * As usual, in a test driven development approach tests normally fail in the
  * beginning. The idea is that the tests will pass in the end.<br>
  * <br/>
- * see: <code>csheets.ext.comments.CommentableCellTest</code><br/>
+ * see: <code>csheets.ext.start_sharing.StartSharingTest</code><br/>
  *
  * <h2>4. Design</h2>
  * To realize this user story we will need to create a subclass of Extension. We
@@ -138,14 +79,10 @@
  * The following diagrams illustrate core aspects of the design of the solution
  * for this use case.<br/>
  *
- * <b>Note:</b> It is very important that in the final version of this technical
- * documentation the elements depicted in these design diagrams exist in the
- * code!</br>
- *
- * <h3>Extension Setup</h3>
- * The following diagram shows the setup of the "comments" extension when
- * cleansheets is run.<br/><br/>
- * <img src="doc-files/core02_01_design.png">
+ * <h3>Start Sharing Diagram</h3>
+ * The following diagram shows the search for other active instances in the same
+ * network, and sends data to that instances .<br/><br/>
+ * <img src="doc-files/ipc_start_sharing_sequence_diagram.png"/>
  * <br/>
  *
  * <h3>User Selects a Cell</h3>
@@ -153,7 +90,7 @@
  * The idea is that when this happens the extension must display in the sidebar
  * the comment of that cell (if it exists).<br/>
  * <br/>
- * <img src="doc-files/core02_01_design2.png">
+ * <img src="doc-files/core02_01_design2.png"/>
  * <br/>
  * <h3>User Updates the Comment of a Cell</h3>
  * The following diagram illustrates what happens when the user updates the text
