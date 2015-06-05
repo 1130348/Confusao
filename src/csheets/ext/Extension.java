f116f1a4091e9090e47a86bc75aa31b642ed5327
1120810 David Alves (1120810DavidAlves) <1120810@isep.ipp.pt>
2015-06-05 13:02:56 +0100

    CSDH-135 #time 2h #comment Task(Tests) completed ahead of schedule #resolve

--- src/csheets/ext/Extension.java
+++ src/csheets/ext/Extension.java
@@ -27,18 +27,29 @@ import csheets.ui.ext.UIExtension;
 
 /**
  * An interface for extensions to the CleanSheets application.
+ *
  * @author Einar Pehrson
  */
 public abstract class Extension implements Comparable<Extension> {
 
-	/** The name of the extension */
+	/**
+	 * The name of the extension
+	 */
 	private final String name;
 
-	/** The base key to use for properties of the extension */
+	/**
+	 * The base key to use for properties of the extension
+	 */
 	private final String basePropKey;
 
 	/**
+	 * The Extension Status
+	 */
+	private boolean status;
+
+	/**
 	 * Creates a new extension.
+	 *
 	 * @param name the name of the extension
 	 */
 	public Extension(String name) {
@@ -46,13 +57,15 @@ public abstract class Extension implements Comparable<Extension> {
 
 		// Builds UI extension base property key
 		String basePropKey = "";
-		for (String token : name.toLowerCase().split(" "))
+		for (String token : name.toLowerCase().split(" ")) {
 			basePropKey += token;
+		}
 		this.basePropKey = basePropKey + ".ui.";
 	}
 
 	/**
 	 * Returns the name of the extension.
+	 *
 	 * @return the name of the extension
 	 */
 	public final String getName() {
@@ -61,6 +74,7 @@ public abstract class Extension implements Comparable<Extension> {
 
 	/**
 	 * Returns the base key to use for properties of the UI extension.
+	 *
 	 * @return the base key to use for properties of the UI extension
 	 */
 	public final String getPropertyKey() {
@@ -68,10 +82,12 @@ public abstract class Extension implements Comparable<Extension> {
 	}
 
 	/**
-	 * Compares this extension with the given extension for order.
-	 * Ordering is done by the extensions' names.
+	 * Compares this extension with the given extension for order. Ordering is
+	 * done by the extensions' names.
+	 *
 	 * @param extension the extension to compared to
-	 * @return a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object.
+	 * @return a negative integer, zero, or a positive integer as this object is
+	 * less than, equal to, or greater than the specified object.
 	 */
 	public final int compareTo(Extension extension) {
 		return name.compareTo(extension.name);
@@ -79,6 +95,7 @@ public abstract class Extension implements Comparable<Extension> {
 
 	/**
 	 * Returns an extension of the given spreadsheet.
+	 *
 	 * @param spreadsheet the spreadsheet to extend
 	 * @return a spreadsheet extension, or null if none is provided
 	 */
@@ -88,6 +105,7 @@ public abstract class Extension implements Comparable<Extension> {
 
 	/**
 	 * Returns an extension of the given cell.
+	 *
 	 * @param cell the cell to extend
 	 * @return a cell extension, or null if none is provided
 	 */
@@ -97,10 +115,30 @@ public abstract class Extension implements Comparable<Extension> {
 
 	/**
 	 * Returns the user interface extension of this extension.
+	 *
 	 * @param uiController the user interface controller
 	 * @return a user interface extension, or null if none is provided
 	 */
 	public UIExtension getUIExtension(UIController uiController) {
 		return null;
 	}
+
+	/**
+	 * Returns the status of this extension.
+	 *
+	 * @return boolean status of this extension
+	 */
+	public boolean Status() {
+		return status;
+	}
+
+	/**
+	 * Defines the status of the extension
+	 *
+	 * @param b sets the new status
+	 */
+	public void defineStatus(boolean b) {
+		this.status = b;
+	}
+
 }
