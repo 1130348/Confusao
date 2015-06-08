6f0d4e79fea07eff3e186d1049479d8deb1787aa
Egidio Santos <1130348@isep.ipp.pt>
2015-06-08 16:48:06 +0100

    Reinserção do codigo retirado para avaliação (CSDH-106)

---
+++ src-tests/csheets/crm/ContactTest.java
@@ -0,0 +1,73 @@
+package csheets.crm;
+
+import csheets.ext.contact.Contact;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * A Unit Test class to test Contact.
+ *
+ * @see Contact
+ * @author Egidio Santos
+ */
+public class ContactTest {
+
+	/**
+	 * A method that tests if the First Name is valid.
+	 */
+	@Test
+	public void testValidaFirstName() {
+
+		Contact contact = new Contact();
+		String fName = "Test";
+		contact.setFirst_Name(fName);
+
+		boolean expResult = true;
+		boolean result = false;
+		if (contact.getFirst_Name().equals(fName)) {
+			result = true;
+		}
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * A method that tests if the Last Name is valid.
+	 */
+	@Test
+	public void testValidaLastName() {
+
+		Contact contact = new Contact();
+		String fName = "Test";
+		contact.setLast_Name(fName);
+
+		boolean expResult = true;
+		boolean result = false;
+		if (contact.getLast_Name().equals(fName)) {
+			result = true;
+		}
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * A method that tests if the image File is valid.
+	 */
+	@Test
+	public void testAddImage() {
+		Contact contact = new Contact();
+		BufferedImage image = new BufferedImage(2, 3, 4);
+		contact.setImage(image);
+
+		boolean expResult = true;
+		boolean result = false;
+
+		if (contact.getImage() == image) {
+			result = true;
+		}
+
+		assertEquals(expResult, result);
+
+	}
+
+}
---
+++ src/csheets/persistence/PersistenceSettings.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csheets.persistence;
+
+/**
+ *
+ * @author rddm
+ */
+class PersistenceSettings {
+
+	/**
+	 * The persistence unit name
+	 */
+	public static final String PERSISTENCE_UNIT_NAME = "cleansheetsPU";
+
+}
