5341c657c0011fea29d7ad0a5e2f0ce0b3b37acb
Marcos <Marcos@Dragon-PC.dei.isep.ipp.pt>
2015-06-09 17:18:39 +0100

    CSDH-150 #comment Add package info #fix it

--- src/csheets/ext/Enable_Disable/package-info.java
+++ src/csheets/ext/Enable_Disable/package-info.java
@@ -2,7 +2,6 @@
  * Provides an extension to support enable and disable extension.
  * 
  * @author Marcos Sousa
- * 
  */
 
 package csheets.ext.Enable_Disable;
--- src/csheets/ext/Enable_Disable/ui/package-info.java
+++ src/csheets/ext/Enable_Disable/ui/package-info.java
@@ -2,7 +2,6 @@
  * Provides the UI of an extension to support enable and disable extension.
  * 
  * @author Marcos Sousa
- * 
  */
 
 package csheets.ext.Enable_Disable.ui;
