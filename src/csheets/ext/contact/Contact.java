98e299748970ab3de1093ef66264ccc810abe61e
1130616 <1130616@isep.ipp.pt>
2015-06-11 15:24:33 +0100

    CSDH-196 #comments adicionados métodos à classe contacto e explicação da expressão regular do numero

--- src/csheets/ext/contact/Contact.java
+++ src/csheets/ext/contact/Contact.java
@@ -1,12 +1,15 @@
 package csheets.ext.contact;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Transient;
 
@@ -22,179 +25,271 @@ import javax.persistence.Transient;
 @Entity
 public class Contact {
 
-	/**
-	 * Id of contact (database)
-	 */
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
+    /**
+     * Id of contact (database)
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
 
-	/**
-	 * The first name of the contact
-	 */
-	private String first_name;
+    /**
+     * The first name of the contact
+     */
+    private String first_name;
 
-	/**
-	 * The last name of the contact
-	 */
-	private String last_name;
+    /**
+     * The last name of the contact
+     */
+    private String last_name;
 
-	/**
-	 * The machine name in order to notify the user about the next events
-	 */
-	private String machine_name;
+    /**
+     * The machine name in order to notify the user about the next events
+     */
+    private String machine_name;
 
-	/**
-	 * The image of the contact
-	 */
-	@Transient
-	private BufferedImage image;
+    /**
+     * The image of the contact
+     */
+    @Transient
+    private BufferedImage image;
 
-	/**
-	 * Agenda of contact
-	 */
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "OWNER_ID")
-	private Agenda agenda;
+    /**
+     * Emails List of the contact
+     */
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Email> listEmail;
 
-	/**
-	 * Constructor without parameters
-	 */
-	public Contact() {
-		agenda = new Agenda();
-		first_name = "";
-		last_name = "";
-		machine_name = "";
-	}
+    /**
+     * Mobile Number of the contact
+     */
+    @OneToOne
+    private PhoneNumber mobileNumber;
 
-	/**
-	 * Constructor with parameters
-	 *
-	 * @param firstN
-	 * @param lastN
-	 * @param machineN
-	 * @param img
-	 */
-	public Contact(String firstN, String lastN, String machineN,
-				   BufferedImage img) {
-		this.first_name = firstN;
-		this.last_name = lastN;
-		this.machine_name = machineN;
-		this.image = img;
-		agenda = new Agenda();
-	}
+    /**
+     * Work Mobile Number of the contact
+     */
+    @OneToOne
+    private PhoneNumber workMobileNumber;
 
-	/**
-	 * Returns the first name of the contact
-	 *
-	 * @return first_name
-	 */
-	public String getFirst_Name() {
-		return this.first_name;
-	}
+    /**
+     * Work Number of the contact
+     */
+    @OneToOne
+    private PhoneNumber workNumber;
 
-	/**
-	 * Returns the last name of the contact
-	 *
-	 * @return last_name
-	 */
-	public String getLast_Name() {
-		return this.last_name;
-	}
+    /**
+     * Home Number of the contact
+     */
+    @OneToOne
+    private PhoneNumber homeNumber;
 
-	/**
-	 * Returns the machine name
-	 *
-	 * @return machine_name
-	 */
-	public String getMachine_Name() {
-		return this.machine_name;
-	}
+    /**
+     * Agenda of contact
+     */
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "OWNER_ID")
+    private Agenda agenda;
 
-	/**
-	 * Returns the image of the contact
-	 *
-	 * @return image
-	 */
-	public BufferedImage getImage() {
-		return this.image;
-	}
+    /**
+     * Constructor without parameters
+     */
+    public Contact() {
+        agenda = new Agenda();
+        first_name = "";
+        last_name = "";
+        machine_name = "";
+        listEmail = new ArrayList<Email>();
+        mobileNumber = new PhoneNumber("");
+        workMobileNumber = new PhoneNumber("");
+        workNumber = new PhoneNumber("");
+        homeNumber = new PhoneNumber("");
+    }
 
-	/**
-	 * Returns the agenda of the contact
-	 *
-	 * @return agenda
-	 */
-	public Agenda getAgenda() {
-		return this.agenda;
-	}
+    /**
+     * Constructor with parameters
+     *
+     * @param first_name
+     * @param last_name
+     * @param machine_name
+     * @param image
+     * @param mobileNumber
+     * @param workMobileNumber
+     * @param workNumber
+     * @param homeNumber
+     */
+    public Contact(String first_name, String last_name, String machine_name, BufferedImage image, PhoneNumber mobileNumber, PhoneNumber workMobileNumber, PhoneNumber workNumber, PhoneNumber homeNumber) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.machine_name = machine_name;
+        this.image = image;
+        this.mobileNumber = mobileNumber;
+        this.workMobileNumber = workMobileNumber;
+        this.workNumber = workNumber;
+        this.homeNumber = homeNumber;
+    }
 
-	/**
-	 * Changes the value of the first name
-	 *
-	 * @param firstN
-	 */
-	public void setFirst_Name(String firstN) {
-		this.first_name = firstN;
-	}
+    /**
+     * Returns the first name of the contact
+     *
+     * @return first_name
+     */
+    public String getFirst_Name() {
+        return this.first_name;
+    }
 
-	/**
-	 * Changes the value of the last name
-	 *
-	 * @param lastN
-	 */
-	public void setLast_Name(String lastN) {
-		this.last_name = lastN;
-	}
+    /**
+     * Returns the last name of the contact
+     *
+     * @return last_name
+     */
+    public String getLast_Name() {
+        return this.last_name;
+    }
 
-	/**
-	 * Changes the value of the machine name
-	 *
-	 * @param machineN
-	 */
-	public void setMachine_Name(String machineN) {
-		this.machine_name = machineN;
-	}
+    /**
+     * Returns the machine name
+     *
+     * @return machine_name
+     */
+    public String getMachine_Name() {
+        return this.machine_name;
+    }
 
-	/**
-	 * Changes the value of the image
-	 *
-	 * @param image
-	 */
-	public void setImage(BufferedImage image) {
-		this.image = image;
-	}
+    /**
+     * Returns the image of the contact
+     *
+     * @return image
+     */
+    public BufferedImage getImage() {
+        return this.image;
+    }
 
-	/**
-	 * Adds a new event to the agenda
-	 *
-	 * @param e event
-	 */
-	public void addEvent(Event e) {
-		agenda.add(e);
-	}
+    /**
+     * Returns the agenda of the contact
+     *
+     * @return agenda
+     */
+    public Agenda getAgenda() {
+        return this.agenda;
+    }
 
-	/**
-	 * Removes a event to the agenda
-	 *
-	 * @param e event
-	 */
-	public void rmvEvent(Event e) {
-		agenda.rmv(e);
-	}
+    /**
+     * Changes the value of the first name
+     *
+     * @param firstN
+     */
+    public void setFirst_Name(String firstN) {
+        this.first_name = firstN;
+    }
 
-	/**
-	 * Edits a event of the agenda
-	 *
-	 * @param e event
-	 */
-	public void editEvent(Event e) {
-		agenda.edit(e);
-	}
+    /**
+     * Changes the value of the last name
+     *
+     * @param lastN
+     */
+    public void setLast_Name(String lastN) {
+        this.last_name = lastN;
+    }
 
-	@Override
-	public String toString() {
-		return getFirst_Name() + " " + getLast_Name();
-	}
+    /**
+     * Changes the value of the machine name
+     *
+     * @param machineN
+     */
+    public void setMachine_Name(String machineN) {
+        this.machine_name = machineN;
+    }
 
+    /**
+     * Changes the value of the image
+     *
+     * @param image
+     */
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    /**
+     * Adds a new event to the agenda
+     *
+     * @param e event
+     */
+    public void addEvent(Event e) {
+        agenda.add(e);
+    }
+
+    /**
+     * Removes a event to the agenda
+     *
+     * @param e event
+     */
+    public void rmvEvent(Event e) {
+        agenda.rmv(e);
+    }
+
+    /**
+     * Edits a event of the agenda
+     *
+     * @param e event
+     */
+    public void editEvent(Event e) {
+        agenda.edit(e);
+    }
+
+    @Override
+    public String toString() {
+        return getFirst_Name() + " " + getLast_Name();
+    }
+
+    /**
+     * @param mobileNumber the mobileNumber to add
+     */
+    public void addMobileNumber(PhoneNumber mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    /**
+     * @param workMobileNumber the workMobileNumber to add
+     */
+    public void addWorkMobileNumber(PhoneNumber workMobileNumber) {
+        this.workMobileNumber = workMobileNumber;
+    }
+
+    /**
+     * @param workNumber the workNumber to add
+     */
+    public void addWorkNumber(PhoneNumber workNumber) {
+        this.workNumber = workNumber;
+    }
+
+    /**
+     * @param homeNumber the homeNumber to add
+     */
+    public void addHomeNumber(PhoneNumber homeNumber) {
+        this.homeNumber = homeNumber;
+    }
+
+    PhoneNumber newPhoneNumber(String newNumber){
+        return new PhoneNumber(newNumber);
+    }
+    
+    public Email newEmail(String text){
+        return new Email(text);
+    }
+    
+    public void addEmail(Email email){
+        listEmail.add(email);
+    }
+    
+    public void removeEmail(Email email){
+        listEmail.remove(email);
+    }
+    
+    public void setPrimaryEmail(Email email){
+        Email tempEmail=listEmail.get(0);
+        removeEmail(email);
+        listEmail.set(0, email);
+        addEmail(tempEmail);
+    }
+    
 }
