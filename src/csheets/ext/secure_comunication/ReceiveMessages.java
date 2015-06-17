3a495affbe831f553113fb2d94ef7982fc23706c
1130616 <1130616@isep.ipp.pt>
2015-06-17 15:02:46 +0100

    Classes para fazer testes funcionais

---
+++ src/csheets/ext/secure_comunication/ReceiveMessages.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csheets.ext.secure_comunication;
+
+import csheets.ext.startsharing.ui.SendCellsAction;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.net.ssl.SSLSocket;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rddm
+ */
+public class ReceiveMessages implements Runnable {
+
+    //private ObjectInputStream objectIn;
+    //private DataInputStream dataIn;
+    Semaphore sem;
+    private SSLSocket socket;
+    private DataInputStream datareader;
+
+    public ReceiveMessages(SSLSocket socket) {
+        try {
+            datareader = new DataInputStream(socket.getInputStream());
+            this.sem = new Semaphore(1);
+            this.socket = socket;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        int nChars;
+        while (true) {
+            try {
+                byte[] data = new byte[300]; 
+                nChars = datareader.read();
+                if (nChars == 0) {
+                    break;
+                }
+                
+                if (nChars > 0) {
+                    datareader.read(data, 0, nChars);
+                    /*  String string = null;
+                    while ((string = bufferedreader.readLine()) != null) {
+                    JOptionPane.showMessageDialog(null, string, "New Message from " + socket.
+                    getInetAddress().
+                    getCanonicalHostName(), JOptionPane.INFORMATION_MESSAGE);
+                    }*/
+                    JOptionPane.showMessageDialog(null, data, "New Message from " + socket.
+                    getInetAddress().
+                    getCanonicalHostName(), JOptionPane.INFORMATION_MESSAGE);
+                    
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/csheets/ext/secure_comunication/SSLServer.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csheets.ext.secure_comunication;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Semaphore;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rddm
+ */
+public class SSLServer implements Runnable {
+
+    boolean isRunning;
+    int port;
+    Map<SSLSocket, DataOutputStream> clientConnections;
+    SSLServerSocket serverSocket;
+    Semaphore sem;
+
+    public SSLServer(int port) {
+        this.port = port;
+        this.clientConnections = new HashMap<>();
+        this.sem = new Semaphore(1);
+        this.isRunning = true;
+    }
+
+    @Override
+    public void run() {
+        int option;
+        try {
+            SSLServerSocketFactory sslserversocketfactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+            serverSocket = (SSLServerSocket) sslserversocketfactory.createServerSocket(9999);
+            SSLSocket client;
+            while (isRunning) {
+                sem.acquire();
+                client = (SSLSocket) serverSocket.accept();
+                option = JOptionPane.showConfirmDialog(null, client.
+                        getInetAddress().
+                        getCanonicalHostName() + " wants to establish a secure comunication, do you accept it?", "Secure comunication request", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+                if (option == 0) {
+                    clientConnections.
+                            put(client, new DataOutputStream(client.
+                                            getOutputStream()));
+
+                    // new Thread(new ReceiveData(client, sendCells)).start();
+                } else {
+                    client.close();
+                }
+                sem.release();
+            }
+        } catch (InterruptedException | IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+}
