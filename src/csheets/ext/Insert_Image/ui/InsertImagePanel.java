61f0af90fa66c054fcc30955edd8be1900f81361
Marcos Sousa (1120836) <1120836@isep.ipp.pt>
2015-06-17 19:12:04 +0100

    CSDH-158 #comment minor image fixes

--- src/csheets/ext/Insert_Image/ui/InsertImagePanel.java
+++ src/csheets/ext/Insert_Image/ui/InsertImagePanel.java
@@ -21,6 +21,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 /**
  *
@@ -82,16 +83,13 @@ public class InsertImagePanel extends JPanel implements SelectionListener, Inser
         });
 
         // Lays out image and button components
-        setLayout(null);
+        setLayout(new BorderLayout());
         l.setBounds(0, 0, 310, 230);
-        add(l);
-        JLabel buttonholder = new JLabel();
-        buttonholder.setBounds(0, 230, 310, 40);
-        buttonholder.setLayout(new FlowLayout());
-        buttonholder.add(in);
-        buttonholder.add(del);
-        add(buttonholder);
-
+        add(l, BorderLayout.CENTER);
+        JPanel button = new JPanel();
+        button.add(in);
+        button.add(del);
+        add(button, BorderLayout.SOUTH);
     }
 
     public void setImage(String Path) {
