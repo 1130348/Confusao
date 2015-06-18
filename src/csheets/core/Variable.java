d6325c416aec7fda1aa0ca4c9a3b6fa976396d72
Sergio <1130399@isep.ipp.pt>
2015-06-18 16:54:56 +0100

    #CSDH-226 #time 2h Macros02.2 - Alteração na implementação, agora quando se cria uma variavel esta posteriormente poderá ser um array, testes totalmente implementados

--- src/csheets/core/Variable.java
+++ src/csheets/core/Variable.java
@@ -6,6 +6,7 @@
 package csheets.core;
 
 import java.io.Serializable;
+import java.util.Vector;
 
 /**
  *
@@ -16,13 +17,28 @@ import java.io.Serializable;
 public class Variable implements Comparable<Variable>, Serializable {
 
 	private String name;
-	private Value value;
-        private Workbook workbook;
 
-	public Variable(String name, Value value, Workbook workbook) {
+	private Value value;
+
+	private Workbook workbook;
+
+	private Vector<Value> positions;
+
+	private int actualPosition;
+
+	public Variable(String name, Value value, Workbook workbook,
+					int actualPosition) {
 		this.name = name;
 		this.value = value;
-                this.workbook=workbook;
+		this.workbook = workbook;
+		this.actualPosition = actualPosition;
+	}
+
+	public void initializePositions() {
+		this.positions = new Vector<Value>();
+		for (int i = 0; i < 10000; i++) {
+			positions.add(new Value(""));
+		}
 	}
 
 	public String getName() {
@@ -32,14 +48,37 @@ public class Variable implements Comparable<Variable>, Serializable {
 	public Value getValue() {
 		return value;
 	}
-        
-        
-    public Workbook getWorkbook() {
-        return workbook;
-    }
+
+	public Workbook getWorkbook() {
+		return workbook;
+	}
 
 	public void setValue(Value value) {
 		this.value = value;
+	}
+
+	public int getActualPosition() {
+		return actualPosition;
+	}
+
+	public void setActualPosition(int actualPosition) {
+		this.actualPosition = actualPosition;
+	}
+
+	public Vector<Value> getPositions() {
+		return positions;
+	}
+
+	public void setPositions(Vector<Value> positions) {
+		this.positions = positions;
+	}
+
+	public Value getPositionValue(int position) {
+		return positions.get(position);
+	}
+
+	public void setPositionValue(int position, Value newValue) {
+		this.positions.add(position, newValue);
 	}
 
 	public int compareTo(Variable otherVariable) {
@@ -49,7 +88,5 @@ public class Variable implements Comparable<Variable>, Serializable {
 
 		return -1;
 	}
-        
-        
 
 }
