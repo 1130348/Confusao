e6430d0f11fce2d45535e7f54f6b8f66853ae855
José Moura (1091334) <1091334@hotmail.com>
2015-06-18 19:19:36 +0100

    CSDH-264 #time 6h #comment Tests completed #resolve

--- src/csheets/ext/style/ui/StyleUIExtension.java
+++ src/csheets/ext/style/ui/StyleUIExtension.java
@@ -21,32 +21,42 @@
  */
 package csheets.ext.style.ui;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JMenu;
-import javax.swing.JToolBar;
-
 import csheets.ext.style.StyleExtension;
 import csheets.ui.ctrl.UIController;
 import csheets.ui.ext.UIExtension;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JMenu;
+import javax.swing.JToolBar;
 
 /**
  * The user interface extension for style.
+ *
  * @author Einar Pehrson
  */
 public class StyleUIExtension extends UIExtension {
 
-	/** The icon to display with the extension's name */
+	private JComponent sideBar;
+
+	/**
+	 * The icon to display with the extension's name
+	 */
 	private Icon icon;
 
-	/** A menu that provides editing of style */
+	/**
+	 * A menu that provides editing of style
+	 */
 	private StyleMenu menu;
 
-	/** A toolbar that provides editing of style */
+	/**
+	 * A toolbar that provides editing of style
+	 */
 	private StyleToolBar toolBar;
 
 	/**
 	 * Creates a new user interface extension for style.
+	 *
 	 * @param extension the extension for which components are provided
 	 * @param uiController the user interface controller
 	 */
@@ -56,32 +66,46 @@ public class StyleUIExtension extends UIExtension {
 
 	/**
 	 * Returns an icon to display with the extension's name.
+	 *
 	 * @return an icon with style
 	 */
 	public Icon getIcon() {
-		if (icon == null)
+		if (icon == null) {
 			icon = new ImageIcon(
 				StyleExtension.class.getResource("res/img/logo.gif"));
+		}
 		return icon;
 	}
 
 	/**
 	 * Returns a menu that provides editing of style.
+	 *
 	 * @return a JMenu component
 	 */
 	public JMenu getMenu() {
-		if (menu == null)
+		if (menu == null) {
 			menu = new StyleMenu(uiController);
+		}
 		return menu;
 	}
 
 	/**
 	 * Returns a toolbar that provides editing of style.
+	 *
 	 * @return a JToolBar component
 	 */
 	public JToolBar getToolBar() {
-		if (toolBar == null)
+		if (toolBar == null) {
 			toolBar = new StyleToolBar(uiController);
+		}
 		return toolBar;
+	}
+
+	@Override
+	public JComponent getSideBar() {
+		if (sideBar == null) {
+			sideBar = new ConditionalFormatingPanel(uiController);
+		}
+		return sideBar;
 	}
 }
