300135f11742fddc2e0b42fb90970d9ee59faa78
1130371 <1130371@isep.ipp.pt>
2015-06-25 14:37:26 +0100

    CSDH-303 #time 1h #comment Testes descomentados e a funcionar #resolve

--- src-tests/csheets/ext/toolbar_buttons/ButtonsControllerTest.java
+++ src-tests/csheets/ext/toolbar_buttons/ButtonsControllerTest.java
@@ -1,5 +1,10 @@
 package csheets.ext.toolbar_buttons;
 
+import csheets.ext.toolbar_buttons.ui.ButtonsController;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 /**
  * The class of tests of the class ButtonsController
  *
@@ -7,74 +12,74 @@ package csheets.ext.toolbar_buttons;
  */
 public class ButtonsControllerTest {
 
-//	/**
-//	 * A method that tests if the method verificationNames is working
-//	 */
-//	@Test
-//	public void testVerificationNamesIcon1() {
-//
-//		ButtonsController b = new ButtonsController();
-//		b.names();
-//
-//		boolean expResult = true;
-//		boolean result = false;
-//		result = b.verificationNames("Smile");
-//		assertEquals(expResult, result);
-//	}
-//
-//	/**
-//	 * A method that tests if the method verificationNames is working
-//	 */
-//	@Test
-//	public void testVerificationNamesIcon2() {
-//
-//		ButtonsController b = new ButtonsController();
-//		b.names();
-//
-//		boolean expResult = false;
-//		boolean result = false;
-//		result = b.verificationNames("Test");
-//		assertEquals(expResult, result);
-//	}
-//
-//	/**
-//	 * A method that tests if the method Names() is working
-//	 */
-//	@Test
-//	public void testVerificationNames() {
-//
-//		ButtonsController b = new ButtonsController();
-//		b.names();
-//
-//		boolean expResult = false;
-//		boolean result = false;
-//		ArrayList<String> iconNames = b.getIconNames();
-//		for (String s : iconNames) {
-//			if (s.equals("Teste")) {
-//				result = true;
-//			}
-//		}
-//		assertEquals(expResult, result);
-//	}
-//
-//	/**
-//	 * A method that tests if the ArrayList iconNames is valid.
-//	 */
-//	@Test
-//	public void testSetGetNamesIcon() {
-//
-//		ButtonsController b = new ButtonsController();
-//
-//		ArrayList<String> iconNames = new ArrayList<String>();
-//		b.setIconNames(iconNames);
-//
-//		boolean expResult = true;
-//		boolean result = false;
-//
-//		if (iconNames == b.getIconNames()) {
-//			result = true;
-//		}
-//
-//		assertEquals(expResult, result);
-//	}
+	/**
+	 * A method that tests if the method verificationNames is working
+	 */
+	@Test
+	public void testVerificationNamesIcon1() {
+
+		ButtonsController b = new ButtonsController();
+		b.names();
+
+		boolean expResult = true;
+		boolean result = false;
+		result = b.verificationNames("Smile");
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * A method that tests if the method verificationNames is working
+	 */
+	@Test
+	public void testVerificationNamesIcon2() {
+
+		ButtonsController b = new ButtonsController();
+		b.names();
+
+		boolean expResult = false;
+		boolean result = false;
+		result = b.verificationNames("Test");
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * A method that tests if the method Names() is working
+	 */
+	@Test
+	public void testVerificationNames() {
+
+		ButtonsController b = new ButtonsController();
+		b.names();
+
+		boolean expResult = false;
+		boolean result = false;
+		ArrayList<String> iconNames = b.getIconNames();
+		for (String s : iconNames) {
+			if (s.equals("Teste")) {
+				result = true;
+			}
+		}
+		assertEquals(expResult, result);
+	}
+
+	/**
+	 * A method that tests if the ArrayList iconNames is valid.
+	 */
+	@Test
+	public void testSetGetNamesIcon() {
+
+		ButtonsController b = new ButtonsController();
+
+		ArrayList<String> iconNames = new ArrayList<String>();
+		b.setIconNames(iconNames);
+
+		boolean expResult = true;
+		boolean result = false;
+
+		if (iconNames == b.getIconNames()) {
+			result = true;
+		}
+
+		assertEquals(expResult, result);
+	}
 }
