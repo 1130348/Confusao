cf43049eadd6853dc9928f10deb1dfb370baf7e4
Carlos Silva <1130664@isep.ipp.pt>
2015-06-26 15:15:24 +0100

    Alteração da implementação para correr em windows

--- src/csheets/ui/sheet/CellRenderer.java
+++ src/csheets/ui/sheet/CellRenderer.java
@@ -35,12 +35,8 @@ import csheets.ui.ext.UIExtension;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.net.URL;
 import java.text.Format;
 import java.util.LinkedList;
-import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
@@ -52,148 +48,152 @@ import javax.swing.table.DefaultTableCellRenderer;
 @SuppressWarnings("serial")
 public class CellRenderer extends DefaultTableCellRenderer {
 
-    /**
-     * The cell decorators invoked by the renderer
-     */
-    private LinkedList<CellDecorator> decorators = new LinkedList<CellDecorator>();
+	/**
+	 * The cell decorators invoked by the renderer
+	 */
+	private LinkedList<CellDecorator> decorators = new LinkedList<CellDecorator>();
 
-    /**
-     * The cell currently being rendered
-     */
-    private Cell cell;
+	/**
+	 * The cell currently being rendered
+	 */
+	private Cell cell;
 
-    /**
-     * Whether the cell currently being rendered is selected
-     */
-    private boolean selected = false;
+	/**
+	 * Whether the cell currently being rendered is selected
+	 */
+	private boolean selected = false;
 
-    /**
-     * Whether the cell currently being rendered has the focus
-     */
-    private boolean hasFocus = false;
+	/**
+	 * Whether the cell currently being rendered has the focus
+	 */
+	private boolean hasFocus = false;
 
-    /**
-     * Creates a new cell renderer.
-     *
-     * @param uiController the user interface controller
-     */
-    public CellRenderer(UIController uiController) {
-        // Fetches decorators
-        for (UIExtension extension : uiController.getExtensions()) {
-            CellDecorator decorator = extension.getCellDecorator();
-            if (decorator != null) {
-                decorators.add(decorator);
-            }
-        }
-    }
+	/**
+	 * Creates a new cell renderer.
+	 *
+	 * @param uiController the user interface controller
+	 */
+	public CellRenderer(UIController uiController) {
+		// Fetches decorators
+		for (UIExtension extension : uiController.getExtensions()) {
+			CellDecorator decorator = extension.getCellDecorator();
+			if (decorator != null) {
+				decorators.add(decorator);
+			}
+		}
+	}
 
-    public Component getTableCellRendererComponent(JTable table, Object o,
-            boolean selected,
-            boolean hasFocus, int row,
-            int column) {
-        super.
-                getTableCellRendererComponent(table, o, selected, hasFocus, row, column);
+	public Component getTableCellRendererComponent(JTable table, Object o,
+												   boolean selected,
+												   boolean hasFocus, int row,
+												   int column) {
+		super.
+			getTableCellRendererComponent(table, o, selected, hasFocus, row, column);
 
-        // Stores members
-        this.cell = (Cell) o;
-        this.selected = selected;
-        this.hasFocus = hasFocus;
+		// Stores members
+		this.cell = (Cell) o;
+		this.selected = selected;
+		this.hasFocus = hasFocus;
 
-        if (cell != null) {
-            // Fetches style
-            StylableCell stylableCell = (StylableCell) cell.
-                    getExtension(StyleExtension.NAME);
+		if (cell != null) {
+			// Fetches style
+			StylableCell stylableCell = (StylableCell) cell.
+				getExtension(StyleExtension.NAME);
 
-            // Applies format and updates text
-            Value value = cell.getValue();
-            Format format = stylableCell.getFormat();
-            setText(format == null ? value.toString() : value.toString(format));
+			// Applies format and updates text
+			Value value = cell.getValue();
+			Format format = stylableCell.getFormat();
+			setText(format == null ? value.toString() : value.toString(format));
 
-            // Applies alignment, font and border
-            setHorizontalAlignment(stylableCell.getHorizontalAlignment());
-            setVerticalAlignment(stylableCell.getVerticalAlignment());
-            setFont(stylableCell.getFont());
-            setBorder(stylableCell.getBorder());
+			// Applies alignment, font and border
+			setHorizontalAlignment(stylableCell.getHorizontalAlignment());
+			setVerticalAlignment(stylableCell.getVerticalAlignment());
+			setFont(stylableCell.getFont());
+			setBorder(stylableCell.getBorder());
 
-            // Applies color
-            if (!selected) {
-                setBackground(stylableCell.getBackgroundColor());
-                if (value.getType() == Value.Type.ERROR) {
-                    setForeground(Color.red);
-                } else {
-                    setForeground(stylableCell.getForegroundColor());
-                }
-            }
+			// Applies color
+			if (!selected) {
+				setBackground(stylableCell.getBackgroundColor());
+				if (value.getType() == Value.Type.ERROR) {
+					setForeground(Color.red);
+				} else {
+					setForeground(stylableCell.getForegroundColor());
+				}
+			}
 
 //                        table = new JTable() {
 //                @Override
 //                public JToolTip createToolTip() {
-//                    
+//
 //                    return new MultiLineToolTip();
 //                }
 //            };
-            // Applies tool tip
-            if (value.getType() == Value.Type.ERROR) {
-                try {
-                    setToolTipText(value.toError().getMessage());
-                } catch (IllegalValueTypeException e) {
-                }
-            }
+			// Applies tool tip
+			if (value.getType() == Value.Type.ERROR) {
+				try {
+					setToolTipText(value.toError().getMessage());
+				} catch (IllegalValueTypeException e) {
+				}
+			}
 
-            CommentableCell commentableCell = (CommentableCell) cell.
-                    getExtension(CommentsExtension.NAME);
+			CommentableCell commentableCell = (CommentableCell) cell.
+				getExtension(CommentsExtension.NAME);
 
-            if (commentableCell.hasComment()) {
+			if (commentableCell.hasComment()) {
 
-                setToolTipText("<html>" + commentableCell.
-                        getAllComments() + "<br>" + "Edited by: " + commentableCell.getUser() + "</html>");
-            } else {
-                setToolTipText(null);
-            }
+				setToolTipText("<html>" + commentableCell.
+					getAllComments() + "<br>" + "Edited by: " + commentableCell.
+					getUser() + "</html>");
+			} else {
+				setToolTipText(null);
+			}
 
-            InsertImageCell imageCell = (InsertImageCell) cell.getExtension(InsertImageExtension.NAME);
-            
-            String OS = System.getProperty("os.name").toLowerCase();
-            
-            if (!commentableCell.hasComment()) {
+			InsertImageCell imageCell = (InsertImageCell) cell.
+				getExtension(InsertImageExtension.NAME);
 
-                if (imageCell.hasImage()) {
-                    if (OS.indexOf("win") > 0) { // verifies if it is windows
-                        setToolTipText("<html><img src=\"file:\\" + imageCell.getImage() + "\"width=\"64\" height=\"64\"></html>");
-                    }else { // it is mac
-                        setToolTipText("<html><img src=\"file://" + imageCell.getImage() + "\"width=\"64\" height=\"64\"></html>");
-                    }
-                } else {
-                    setToolTipText(null);
-                }
-            } else {
-                if (imageCell.hasImage()) {
-                    if (OS.indexOf("win") > 0) { // verifies if it is windows
-                        setToolTipText("<html><img src=\"file:\\" + imageCell.getImage() + "\"width=\"64\" height=\"64\"></html>");
-                    }else { // it is mac
-                        setToolTipText("<html><img src=\"file://" + imageCell.getImage() + "\"width=\"64\" height=\"64\"></html>");
-                    }
-                }
-            }
-        }
-        return this;
-    }
+			String OS = System.getProperty("os.name").toLowerCase();
 
-    /**
-     * Overridden to delegate painting to decorators.
-     *
-     * @param g the Graphics object to protect
-     */
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+			if (!commentableCell.hasComment()) {
 
-        if (cell != null) // Invokes decorators
-        {
-            for (CellDecorator decorator : decorators) {
-                if (decorator.isEnabled()) {
-                    decorator.decorate(this, g, cell, selected, hasFocus);
-                }
-            }
-        }
-    }
+				if (imageCell.hasImage()) {
+//                    if (OS.indexOf("win") > 0) { // verifies if it is windows
+					setToolTipText("<html><img src=\"file:\\" + imageCell.
+						getImage() + "\"width=\"64\" height=\"64\"></html>");
+//                    }else { // it is mac
+//                        setToolTipText("<html><img src=\"file://" + imageCell.getImage() + "\"width=\"64\" height=\"64\"></html>");
+//                    }
+				} else {
+					setToolTipText(null);
+				}
+			} else {
+				if (imageCell.hasImage()) {
+//                    if (OS.indexOf("win") > 0) { // verifies if it is windows
+					setToolTipText("<html><img src=\"file:\\" + imageCell.
+						getImage() + "\"width=\"64\" height=\"64\"></html>");
+//                    }else { // it is mac
+//                        setToolTipText("<html><img src=\"file://" + imageCell.getImage() + "\"width=\"64\" height=\"64\"></html>");
+//                    }
+				}
+			}
+		}
+		return this;
+	}
+
+	/**
+	 * Overridden to delegate painting to decorators.
+	 *
+	 * @param g the Graphics object to protect
+	 */
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		if (cell != null) // Invokes decorators
+		{
+			for (CellDecorator decorator : decorators) {
+				if (decorator.isEnabled()) {
+					decorator.decorate(this, g, cell, selected, hasFocus);
+				}
+			}
+		}
+	}
 }
